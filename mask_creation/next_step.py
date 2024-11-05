da6b1cfb13ff1788c4baa067767280a8cac8fac0
francesco <fjm22@ic.ac.uk>
2024-11-05 11:55:02 +0000

    starting to create the mask

--- mask_creation/next_step.py
+++ mask_creation/next_step.py
@@ -1,33 +1,19 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
-
+from scipy.ndimage import gaussian_filter, label
+# from photutils import aperture_photometry, CircularAperture
 path="fits_file/mosaic.fits"
-#parameters to calcualte background:
-num_bins = 10000
+#get threshold from other program!!!!!!!!
+threshold = 3481
 
-
-def gaussian(x, a, std, m):
-    return a *(1/(np.sqrt(2*np.pi)*std))* np.exp(-0.5 * ((x - m) / std) ** 2)
-#open file
+#open file and get data
 hdulist = fits.open(path)
-
 data = hdulist[0].data
 
-# Calculate histogram bin counts and bin edges
-counts, bin_edges = np.histogram(data.ravel(), bins=num_bins)
-
-# Calculate bin centers as the midpoint between each bin edge
-bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
 #create mask
-data_copy = np.copy(data)
+mask= data > threshold
 
-
-
-
-
-
-#close file
+#close fits file
 hdulist.close()
