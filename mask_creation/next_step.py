e4c620234acb4d1304db7a83ab65493a24228e9e
francesco <fjm22@ic.ac.uk>
2024-11-05 12:40:17 +0000

    created binary mask

--- mask_creation/next_step.py
+++ mask_creation/next_step.py
@@ -1,7 +1,9 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import gaussian_filter, label
+from scipy.ndimage import label, find_objects
+import pandas as pd
+
 # from photutils import aperture_photometry, CircularAperture
 path="fits_file/mosaic.fits"
 #get threshold from other program!!!!!!!!
@@ -13,7 +15,9 @@ data = hdulist[0].data
 
 
 #create mask
-mask= data > threshold
+binary_mask= data > threshold
+
+print(binary_mask)
 
 #close fits file
 hdulist.close()
